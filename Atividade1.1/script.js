bef4dc323389ffcb135dddfec7868ad9215d1343
Diego Alves <diegoteialves@gmail.com>
2023-02-06 20:14:22 -0300

    Atividade 1.1

---
+++ Atividade1.1/script.js
@@ -0,0 +1,21 @@
+var soma = 0
+soma = 7 + 2
+
+var diferenca
+diferenca = 8 - 4
+
+var divisao
+divisao = 80 / 3
+
+var resto
+resto = 80 % 3
+
+soma++
+soma++
+soma++
+soma++
+soma++
+
+soma--
+soma--
+soma--
