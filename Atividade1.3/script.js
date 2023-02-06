5ecc3449b93aa79ad86f54307d7effc90289b21a
Diego Alves <diegoteialves@gmail.com>
2023-02-06 20:52:43 -0300

    Atividade 1.3

---
+++ Atividade1.3/script.js
@@ -0,0 +1,18 @@
+var caminho = 5
+var corro = 2
+if(caminho == 5 && corro == 2){
+    console.log("Passo o dia bem")
+}
+
+var levanto = "cedo"
+
+if(levanto == "cedo" || levanto == "antes das 8h"){
+    console.log("Saio tranquilo")
+}
+
+termino = "nao"
+compromisso = "tem"
+
+if(termino != "nao" || compromisso != "tem"){
+    console.log("saio tarde")
+} 
